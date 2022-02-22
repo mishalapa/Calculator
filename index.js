d184db23b58ad6fa71da6862037920fc92d4167d
mishalapa <mishamedme@gmail.com>
2022-02-22 10:46:29 +0300

    feat: add new doc, style for calculator, grid position for button

---
+++ index.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-clear]')
+const beforeButton = document.querySelector('[data-before]')
+const nowButton = document.querySelector('[data-now]')
