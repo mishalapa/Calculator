e5f2238133bc3e24933eba250ec013a3ff0c9b02
mishalapa <mishamedme@gmail.com>
2022-02-22 12:54:35 +0300

    feat: add all keydown for calculator

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ class Calculator {
     }
 
     delete() {
-
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
 
     appendNumber(number) {
         if(number === '.' && this.currentOperand.includes('.')) return
         this.currentOperand = this.currentOperand.toString() + number.toString();
     }
-
+   
     chooseOperation(operation) {
         if(!this.currentOperand) return
         if(this.previousOperand !== '') {
@@ -103,3 +103,89 @@ allClearButton.addEventListener('click', () => {
     calculator.clear()
     calculator.updateDisplay()
 })
+
+deleteButton.addEventListener('click', () => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
+
+  const body = document.body
+  body.addEventListener('keydown', newWordSearchEnter);
+
+  function newWordSearchEnter(e)  {
+    if (e.keyCode === 96) {
+        calculator.appendNumber(0)
+        calculator.updateDisplay()
+    }
+    if (e.keyCode === 97) {
+        calculator.appendNumber(1)
+        calculator.updateDisplay()
+    }
+    if (e.keyCode === 98) {
+        calculator.appendNumber(2)
+        calculator.updateDisplay()
+    }
+    if (e.keyCode === 99) {
+        calculator.appendNumber(3)
+        calculator.updateDisplay()
+    }
+    if (e.keyCode === 100) {
+        calculator.appendNumber(4)
+        calculator.updateDisplay()
+    }
+    if (e.keyCode === 101) {
+        calculator.appendNumber(5)
+        calculator.updateDisplay()
+    }
+    if (e.keyCode === 102) {
+        calculator.appendNumber(6)
+        calculator.updateDisplay()
+    }
+    if (e.keyCode === 103) {
+        calculator.appendNumber(7)
+        calculator.updateDisplay()
+    }
+    if (e.keyCode === 104) {
+        calculator.appendNumber(8)
+        calculator.updateDisplay()
+    }
+    if (e.keyCode === 105) {
+        let number = 9;
+        calculator.appendNumber(9)
+        calculator.updateDisplay()
+    }
+    if (e.keyCode === 106) {
+        calculator.chooseOperation('*')
+        calculator.updateDisplay()
+    }
+    if (e.keyCode === 107) {
+        calculator.chooseOperation('+')
+        calculator.updateDisplay()
+    }
+    if (e.keyCode === 109) {
+        calculator.chooseOperation('-')
+        calculator.updateDisplay()
+    }
+    if (e.keyCode === 110) {
+        let number = '.';
+        calculator.appendNumber('.')
+        calculator.updateDisplay()
+    }
+    if (e.keyCode === 111) {
+        calculator.chooseOperation('÷')
+        calculator.updateDisplay()
+    }
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        calculator.compute()
+        calculator.updateDisplay()
+    }
+    if (e.keyCode === 46) {
+        calculator.delete()
+        calculator.updateDisplay()
+    }
+    if (e.keyCode === 8) {
+        calculator.clear()
+        calculator.updateDisplay()
+    }
+  }
