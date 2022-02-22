3cfc27a01a27b6466026064e7022f84a7325699e
mishalapa <mishamedme@gmail.com>
2022-02-22 13:24:23 +0300

    feat: add adaptive

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ class Calculator {
     }
 
     appendNumber(number) {
+        if(this.currentOperand.length > 56) return
         if(number === '.' && this.currentOperand.includes('.')) return
         this.currentOperand = this.currentOperand.toString() + number.toString();
     }
